cc3f697c7ee55613d07f4b25df134002d9094802
hyansmn <hyankelwin@hotmail.com>
2019-09-03 22:32:38 -0300

    03/09

---
+++ bin/README.md
@@ -0,0 +1,3 @@
+[![Build Status](https://travis-ci.org/PedroGaletti/my-java-spring.svg?branch=master)](https://travis-ci.org/PedroGaletti/my-java-spring)
+# my-first-project-spring
+My first project Spring Boot
---
+++ src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
@@ -0,0 +1,30 @@
+package br.com.unifacef.api;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.com.unifacef.api.utils.SenhaUtils;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoSpringApplication.class, args);
+		System.out.println("Alteramos o arquivo application.properties ");
+		System.out.println("de modo que não precisemos configurar agora a fonte de dados do MySQL ");
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded: " + senhaEncoded);
+			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded novamente:" + senhaEncoded);
+			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", 
+					senhaEncoded));
+		};
+	}
+}
